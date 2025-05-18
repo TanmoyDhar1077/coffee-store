f3cfb7d2d8c535e852fd4e35e8ec1601b352595e
TanmoyDhar1077 <tanmoydhar1077@gmail.com>
2025-05-18 14:03:29 +0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl">Hello Tea lover</h1>
+      <button className="btn raleway"> Add me</button>
+    </>
+  );
+}
+
+export default App;
